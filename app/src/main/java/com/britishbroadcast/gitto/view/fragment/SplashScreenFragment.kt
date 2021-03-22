d0868d86fa86ec6ab4da17c3e97ebc5ea05a43a4
lightScout <juanmullercs@hotmail.com>
2021-03-22 10:25:32 +0000

    .add SplashScreenFragment.kt - op

---
+++ app/src/main/java/com/britishbroadcast/gitto/view/fragment/SplashScreenFragment.kt
@@ -0,0 +1,44 @@
+package com.britishbroadcast.gitto.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.britishbroadcast.gitto.R
+import com.britishbroadcast.gitto.databinding.SplashScreenLayoutBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+
+class SplashScreenFragment: Fragment() {
+
+    private lateinit var binding: SplashScreenLayoutBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = SplashScreenLayoutBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            logoImageView.setImageResource(R.drawable.logo)
+        }
+
+        //Coroutine
+        GlobalScope.launch {
+            delay(2000L)
+            // Thi is where we will call the login page
+            // For now it is only popBackStack
+            parentFragmentManager.popBackStack()
+        }
+
+
+    }
+
+
+}
--- app/src/main/java/com/britishbroadcast/gitto/view/ui/MainActivity.kt
+++ app/src/main/java/com/britishbroadcast/gitto/view/ui/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.britishbroadcast.gitto.view.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.britishbroadcast.gitto.R
 import com.britishbroadcast.gitto.model.network.GittoRetrofit
+import com.britishbroadcast.gitto.view.fragment.SplashScreenFragment
 import com.britishbroadcast.gitto.viewmodel.GittoViewModel
 
 class MainActivity : AppCompatActivity() {
     private val gittoViewModel = GittoViewModel()
+    private val splashScreenFragment = SplashScreenFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         gittoViewModel.getGitUser("lightscout")
+
+        supportFragmentManager.beginTransaction()
+                .setCustomAnimations(
+                        android.R.anim.fade_in,
+                        android.R.anim.fade_out,
+                        android.R.anim.fade_in,
+                        android.R.anim.fade_out
+                ).replace(R.id.main_frameLayout, splashScreenFragment)
+                .addToBackStack(null)
+                .commit()
 
     }
 }
